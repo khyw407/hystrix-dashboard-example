a3b91db397d873f03fc7349fe4b3500cb014da0a
Hyunwoo Kim <khyw407@gmail.com>
2018-12-07 16:30:48 +0900

    first commit about hystirx dashboard

---
+++ src/main/java/com/test/hystrixdashborad/HystrixDashboradApplication.java
@@ -0,0 +1,14 @@
+package com.test.hystrixdashborad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboradApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HystrixDashboradApplication.class, args);
+	}
+}
---
+++ src/test/java/com/test/hystrixdashborad/HystrixDashboradApplicationTests.java
@@ -0,0 +1,16 @@
+package com.test.hystrixdashborad;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HystrixDashboradApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
