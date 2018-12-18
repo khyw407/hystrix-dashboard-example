a784701f8d0053d98ed31d3192b74224ac9bf659
Hyunwoo Kim <khyw407@gmail.com>
2018-12-18 12:02:47 +0900

    fix annotation and dependency

--- src/main/java/com/test/hystrixdashborad/HystrixDashboradApplication.java
+++ src/main/java/com/test/hystrixdashborad/HystrixDashboradApplication.java
@@ -4,12 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
-import org.springframework.cloud.netflix.turbine.EnableTurbine;
+import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
 
 @SpringBootApplication
 @EnableHystrixDashboard
 @EnableEurekaClient
-@EnableTurbine
+@EnableTurbineStream
 public class HystrixDashboradApplication {
 
 	public static void main(String[] args) {
